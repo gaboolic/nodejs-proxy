a3e92553b7186e7c4a23750f6ba1e44f556f0abb
gaboolic <23441099@qq.com>
2023-07-20 16:50:53 +0800

    tunnel.js

---
+++ tunnel.js
@@ -0,0 +1,31 @@
+const net=require('net');
+const {WebSocket,createWebSocketStream}=require('ws');
+const logcb= (...args)=>console.log.bind(this,...args);
+const errcb= (...args)=>console.error.bind(this,...args);
+
+const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replaceAll('-','');
+const port= process.env.PORT||7860;
+
+const wss=new WebSocket.Server({port},logcb('listen:', port));
+wss.on('connection', ws=>{
+    console.log("on connection")
+    ws.once('message', msg=>{
+        const [VERSION]=msg;
+        const id=msg.slice(1, 17);
+        if(!id.every((v,i)=>v==parseInt(uuid.substr(i*2,2),16))) return;
+        let i = msg.slice(17, 18).readUInt8()+19;
+        const port = msg.slice(i, i+=2).readUInt16BE(0);
+        const ATYP = msg.slice(i, i+=1).readUInt8();
+        const host= ATYP==1? msg.slice(i,i+=4).join('.')://IPV4
+            (ATYP==2? new TextDecoder().decode(msg.slice(i+1, i+=1+msg.slice(i,i+1).readUInt8()))://domain
+                (ATYP==3? msg.slice(i,i+=16).reduce((s,b,i,a)=>(i%2?s.concat(a.slice(i-1,i+1)):s), []).map(b=>b.readUInt16BE(0).toString(16)).join(':'):''));//ipv6
+
+        logcb('conn:', host,port);
+        ws.send(new Uint8Array([VERSION, 0]));
+        const duplex=createWebSocketStream(ws);
+        net.connect({host,port}, function(){
+            this.write(msg.slice(i));
+            duplex.on('error',errcb('E1:')).pipe(this).on('error',errcb('E2:')).pipe(duplex);
+        }).on('error',errcb('Conn-Err:',{host,port}));
+    }).on('error',errcb('EE:'));
+});
