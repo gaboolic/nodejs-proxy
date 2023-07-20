ba9deb2eae48fe1b55cdef953ce99c4e9d90cc8a
gaboolic <23441099@qq.com>
2023-07-20 17:07:05 +0800

    for render

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
 const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replaceAll('-','');
-const port= process.env.PORT||3001;
+const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
 wss.on('connection', ws=>{
